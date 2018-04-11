40d6a6fb6030007f8b340ec5fc5110c972171234
Damjan Vučina <damjan.vucina@fer.hr>
2018-04-11 10:48:55 +0200

    Debugged Task4 subtask7

---
+++ hw05-0036485908/src/main/java/hr/fer/zemris/java/hw05/db/QueryParser.java
@@ -0,0 +1,136 @@
+package hr.fer.zemris.java.hw05.db;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class QueryParser {
+	private static final String VALID_JMBAG = "\\d{10}";
+	private static final String QUERY_SPLITTER = "(?i:AND)";
+	private static final String VALID_NAME = "[A-Za-z]+";
+
+	String query;
+	List<ConditionalExpression> expressions;
+
+	public QueryParser(String query) {
+		this.query = query;
+		expressions = new LinkedList<>();
+
+		parse();
+	}
+
+	public boolean isDirectQuery() {
+		String queryCopy = query.replace(" ", "");
+
+		if (queryCopy.startsWith("jmbag=")) {
+			queryCopy = queryCopy.substring("jmbag=".length() + 1, queryCopy.length() - 1);
+
+			return queryCopy.matches(VALID_JMBAG);
+
+		}
+		return false;
+	}
+
+	public String getQueriedJMBAG() {
+		if (!isDirectQuery()) {
+			throw new IllegalStateException("Last performed query was not a direct query.");
+		}
+		String queryCopy = query.replace(" ", "");
+
+		return queryCopy.substring("jmbag=\"".length(), queryCopy.length() - 1);
+
+	}
+
+	public List<ConditionalExpression> getQuery() {
+		return expressions;
+	}
+
+	private void parse() {
+		String queryCopy = query.replace(" ", "");
+		String[] queries = queryCopy.split(QUERY_SPLITTER);
+
+		for (String singleQuery : queries) {
+			if (singleQuery.startsWith("firstName")) {
+				parseAttribute(singleQuery, "firstName");
+
+			} else if (singleQuery.startsWith("lastName")) {
+				parseAttribute(singleQuery, "lastName");
+
+			} else if (singleQuery.startsWith("jmbag")) {
+				parseAttribute(singleQuery, "jmbag");
+
+			} else {
+				throw new IllegalArgumentException("Unsupported query operation.");
+			}
+		}
+	}
+
+	private void parseAttribute(String queryCopy, String attribute) {
+		IComparisonOperator comparisonOperator = validateComparisonOperator(
+				queryCopy.substring(attribute.length(), queryCopy.indexOf("\"")));
+		IFieldValueGetter fieldGetter = validateFieldGetter(attribute);
+		String stringLiteral = validateStringLiteral(
+				queryCopy.substring(queryCopy.indexOf("\"") + 1, queryCopy.lastIndexOf("\"")), attribute);
+
+		expressions.add(new ConditionalExpression(fieldGetter, stringLiteral, comparisonOperator));
+	}
+
+	private String validateStringLiteral(String stringLiteral, String attribute) {
+		if (attribute.equals("firstName") || attribute.equals("lastName")) {
+			if (stringLiteral.matches(VALID_NAME)) {
+				return stringLiteral;
+			}
+
+		} else if (attribute.equals("jmbag")) {
+			if (stringLiteral.matches(VALID_JMBAG)) {
+				return stringLiteral;
+			}
+		}
+
+		throw new IllegalArgumentException("Invalid stringLiteral, was: " + stringLiteral);
+	}
+
+	private IFieldValueGetter validateFieldGetter(String attribute) {
+		switch (attribute) {
+		case "firstName":
+			return FieldValueGetters.FIRST_NAME;
+
+		case "lastName":
+			return FieldValueGetters.LAST_NAME;
+
+		case "jmbag":
+			return FieldValueGetters.JMBAG;
+
+		default:
+			throw new IllegalArgumentException("Unsupported field getter, was: " + attribute);
+		}
+	}
+
+	private IComparisonOperator validateComparisonOperator(String comparisonOperator) {
+		switch (comparisonOperator) {
+		case "<":
+			return ComparisonOperators.LESS;
+
+		case "<=":
+			return ComparisonOperators.LESS_OR_EQUALS;
+
+		case ">":
+			return ComparisonOperators.GREATER;
+
+		case ">=":
+			return ComparisonOperators.GREATER_OR_EQUALS;
+
+		case "=":
+			return ComparisonOperators.EQUALS;
+
+		case "!=":
+			return ComparisonOperators.NOT_EQUALS;
+
+		case "LIKE":
+			return ComparisonOperators.LIKE;
+
+		default:
+			throw new IllegalArgumentException("Unsupported operator occured, was: " + comparisonOperator);
+		}
+	}
+
+}
---
+++ hw05-0036485908/src/test/java/hr/fer/zemris/java/hw05/db/QueryParserTest.java
@@ -0,0 +1,33 @@
+package hr.fer.zemris.java.hw05.db;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QueryParserTest {
+	
+	QueryParser queryParser;
+	
+	@Test
+	public void templateTestDirect() {
+		 queryParser = new QueryParser(" jmbag =\"0123456789\" ");
+		 
+		 Assert.assertTrue(queryParser.isDirectQuery());
+		 Assert.assertEquals("0123456789", queryParser.getQueriedJMBAG());
+		 Assert.assertEquals(1, queryParser.getQuery().size());
+	}
+	
+	@Test
+	public void templateTestNonDirect() {
+		 QueryParser queryParser = new QueryParser("jmbag=\"0123456789\" and lastName>\"J\"");
+		 
+		 Assert.assertFalse(queryParser.isDirectQuery());
+		 Assert.assertEquals(2, queryParser.getQuery().size());
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void templateGetQueriedJmbagAfterNonDirectQuery() {
+		 QueryParser queryParser = new QueryParser("jmbag=\"0123456789\" and lastName>\"J\"");
+		 queryParser.getQueriedJMBAG(); // would throw!
+	}
+
+}
