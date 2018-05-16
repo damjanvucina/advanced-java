33a98bbbdb7dc7b0c7742e848fdf0f3e54113b0c
Damjan Vučina <damjan.vucina@fer.hr>
2018-05-16 18:00:41 +0200

    Documented Task4

--- hw10-0036485908/src/main/java/hr/fer/zemris/java/gui/prim/PrimDemo.java
+++ hw10-0036485908/src/main/java/hr/fer/zemris/java/gui/prim/PrimDemo.java
@@ -12,47 +12,63 @@ import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
+/**
+ * Demonstration class used for previewing the constructed window printing prime
+ * numbers.
+ * 
+ * @author Damjan Vučina
+ */
 public class PrimDemo extends JFrame {
+
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Instantiates a new prim demo.
+	 */
 	public PrimDemo() {
 		setLocation(50, 50);
 		setSize(500, 500);
 		setTitle("Prim Demo");
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		
+
 		initGUI();
 	}
-	
+
+	/**
+	 * Initializes the GUI.
+	 */
 	public void initGUI() {
 		Container cp = getContentPane();
 		cp.setLayout(new BorderLayout());
-		
+
 		PrimListModel model = new PrimListModel();
-		
+
 		JList<Integer> list1 = new JList<>(model);
 		JList<Integer> list2 = new JList<>(model);
-		
-		
-		
+
 		JPanel listPanel = new JPanel(new GridLayout(1, 0));
 		listPanel.add(new JScrollPane(list1));
 		listPanel.add(new JScrollPane(list2));
-		
+
 		JPanel buttonPanel = new JPanel(new GridLayout(1, 0));
 		JButton addButton = new JButton("Sljedeći");
 		addButton.addActionListener(l -> {
 			model.next();
-			
+
 		});
 		buttonPanel.add(addButton);
-		
 
-		
 		cp.add(listPanel, BorderLayout.CENTER);
 		cp.add(buttonPanel, BorderLayout.PAGE_END);
 	}
-	
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
 	public static void main(String[] args) {
 
 		SwingUtilities.invokeLater(() -> {
--- hw10-0036485908/src/main/java/hr/fer/zemris/java/gui/prim/PrimListModel.java
+++ hw10-0036485908/src/main/java/hr/fer/zemris/java/gui/prim/PrimListModel.java
@@ -10,55 +10,92 @@ import javax.swing.ListModel;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
+/**
+ * The class used for creating a window containing two least each of them
+ * printing a next prime number on a click of the designated button.
+ * 
+ * @author Damjan Vučina
+ */
 public class PrimListModel implements ListModel<Integer> {
+
+	/** The Constant START. */
 	private static final int START = 1;
-	
+
+	/** The list of processed primes. */
 	private List<Integer> primes = new ArrayList<>();
+
+	/** The registered listeners. Observer pattern is in action. */
 	private List<ListDataListener> listeners = new ArrayList<>();
 
+	/**
+	 * Instantiates a new prim list model.
+	 */
 	public PrimListModel() {
 		next();
 	}
+
+	/**
+	 * Adds a listener to the list that's notified each time a change to the data
+	 * model occurs.
+	 */
 	@Override
 	public void addListDataListener(ListDataListener l) {
 		Objects.requireNonNull(l, "Listener cannot be null");
-		
+
 		listeners.add(l);
 	}
 
+	/**
+	 * Returns the value at the specified index.
+	 */
 	@Override
 	public Integer getElementAt(int index) {
-		if(index < 0 || index > getSize() -1) {
+		if (index < 0 || index > getSize() - 1) {
 			throw new IllegalArgumentException("Invalid index, was:" + index);
 		}
 		return primes.get(index);
 	}
 
+	/**
+	 * Returns the length of the list.
+	 */
 	@Override
 	public int getSize() {
 		return primes.size();
 	}
 
+	/**
+	 * Removes a listener from the list that's notified each time a change to the
+	 * data model occurs.
+	 */
 	@Override
 	public void removeListDataListener(ListDataListener l) {
 		Objects.requireNonNull(l, "Listener cannot be null");
-		
+
 		listeners.remove(l);
 	}
 
+	/**
+	 * Acquires the next prime number and notifies observers.
+	 */
 	public void next() {
 		int position = primes.size();
 		primes.add(position == 0 ? START : generateNextPrime());
-		
+
 		ListDataEvent event = new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, position, position);
 		for (ListDataListener l : listeners) {
 			l.intervalAdded(event);
 		}
 	}
 
+	/**
+	 * Generates next prime number.
+	 *
+	 * @return the next prime number
+	 */
 	private int generateNextPrime() {
 		int lastPrime = primes.get(primes.size() - 1);
-		
+
 		for (int current = lastPrime + 1;; current++) {
 			if (isPrime(current)) {
 				return current;
@@ -66,12 +103,19 @@ public class PrimListModel implements ListModel<Integer> {
 		}
 
 	}
-	
+
+	/**
+	 * Checks if the number is prime.
+	 *
+	 * @param number
+	 *            the number
+	 * @return true, if is prime
+	 */
 	public boolean isPrime(int number) {
-		if(number < 1) {
+		if (number < 1) {
 			throw new IllegalArgumentException("Number cannot be lesser than one.");
 		}
-		
+
 		if (number != 2 && number % 2 == 0) {
 			return false;
 		}
