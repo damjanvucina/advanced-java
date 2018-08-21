8d8dbbb60a465b92d397b9a1cdfa197ce5409f9b
Damjan Vučina <damjan.vucina@fer.hr>
2018-08-21 14:03:54 +0200

    Debugged SaveAsAction

--- hw16-0036485908-2/src/main/java/hr/fer/zemris/java/hw16/jvdraw/actions/SaveAsAction.java
+++ hw16-0036485908-2/src/main/java/hr/fer/zemris/java/hw16/jvdraw/actions/SaveAsAction.java
@@ -5,36 +5,55 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import hr.fer.zemris.java.hw16.jvdraw.JVDraw;
 
 public class SaveAsAction extends AbstractAction {
 	private static final long serialVersionUID = 1L;
+	private static final String JVD_EXTENSION = ".jvd";
 
 	private JVDraw window;
+	private FileNameExtensionFilter jvdFilter;
 
 	public SaveAsAction(JVDraw window) {
 		this.window = window;
+		
+		jvdFilter = new FileNameExtensionFilter(".jvd", "jvd");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JFileChooser jfc = new JFileChooser();
 		jfc.setDialogTitle("Save document");
+		jfc.setFileFilter(jvdFilter);
 		int dialogResult = jfc.showSaveDialog(window);
 
 		if (dialogResult == JFileChooser.CANCEL_OPTION) {
 			JOptionPane.showMessageDialog(window, "Saving cancelled.", "Info", JOptionPane.INFORMATION_MESSAGE);
 			return;
 
-		} else if (dialogResult == JFileChooser.APPROVE_OPTION) {
-
+		} 
+		
+		Path savePath = jfc.getSelectedFile().toPath();
+		if(isInvalidPath(savePath)) {
+			JOptionPane.showMessageDialog(window, "Requested file name is not valid. Supported file extension: .jvd",
+												  "Invalid file name", JOptionPane.INFORMATION_MESSAGE);
+			return;
+		}
+		if(extensionNotSet(savePath)) {
+			savePath = Paths.get(String.valueOf(savePath) + JVD_EXTENSION);
+		}
+		
+		if (dialogResult == JFileChooser.APPROVE_OPTION) {
 			int overwriteResult = 0;
-			if (Files.exists(jfc.getSelectedFile().toPath())) {
+			if (Files.exists(savePath)) {
 				overwriteResult = JOptionPane.showConfirmDialog(jfc,
 						"File already exists. Do you want to overwrite it?", "File exists", JOptionPane.YES_NO_OPTION);
 				if (overwriteResult == JOptionPane.NO_OPTION) {
@@ -45,8 +64,7 @@ public class SaveAsAction extends AbstractAction {
 
 		String jvdRepresentation = UtilityProvider.toJVD(window.getDocumentModel().getObjects());
 		byte[] bytes = jvdRepresentation.getBytes(StandardCharsets.UTF_8);
-		Path savePath = jfc.getSelectedFile().toPath();
-
+		
 		try {
 			Files.write(savePath, bytes);
 		} catch (IOException e1) {
@@ -55,5 +73,18 @@ public class SaveAsAction extends AbstractAction {
 		
 		JOptionPane.showMessageDialog(window, "File saved successfully.", "File Saved", JOptionPane.INFORMATION_MESSAGE);
 	}
+
+	private boolean extensionNotSet(Path savePath) {
+		return !String.valueOf(savePath).contains(".");
+	}
+
+	//@formatter:off
+	private boolean isInvalidPath(Path path) {
+		String p = String.valueOf(path);
+		int numOfDots = p.length() - p.replace(".", "").length();
+		
+		return numOfDots > 1 || (numOfDots == 1 && !p.endsWith(JVD_EXTENSION));
+	}
+	//@formatter:on
 
 }
